f70436c2d9c20dd5ab15fd3c083b91974b0c0162
Budig <1>
2017-03-29 15:03:39 +0200

    PC-Tests

--- PuvS/src/assignment02/Prefix.java
+++ PuvS/src/assignment02/Prefix.java
@@ -24,32 +24,32 @@ public class Prefix {
 		Arrays.sort(times);
 		System.out.println("Executor-Median, 4 Threads: " + times[0]);
 		
-		ParallelPrefixExec pe2 = new ParallelPrefixExec(length, 4);
+		ParallelPrefixExec pe2 = new ParallelPrefixExec(length, 8);
 		long[] times3 = new long[max];
 		for (int i = 0; i < max; i++) {
 			pe2.resetArray();
 			start = System.nanoTime();
-			pe2.parallelPrefix(200);
+			pe2.parallelPrefix(16);
 			end = System.nanoTime();
 			long exTime = end - start;
 			times3[i] = exTime;
 		}
 		pe2.shutdown();
 		Arrays.sort(times3);
-		System.out.println("Executor-Median, 200 Threads: " + times3[0]);
+		System.out.println("Executor-Median, 16 Threads: " + times3[0]);
 //
-		long[] times2 = new long[max];
-		for (int i = 0; i < max; i++) {
-			pp.initializeArray(length);
-			start = System.nanoTime();
-			pp.seqPrefixSum();
-			end = System.nanoTime();
-			long seqTime = end - start;
-			times2[i] = seqTime;
-		}
-		Arrays.sort(times2);
-		System.out.println("Seq-Median: " + times2[0]);
-		
+//		long[] times2 = new long[max];
+//		for (int i = 0; i < max; i++) {
+//			pp.initializeArray(length);
+//			start = System.nanoTime();
+//			pp.seqPrefixSum();
+//			end = System.nanoTime();
+//			long seqTime = end - start;
+//			times2[i] = seqTime;
+//		}
+//		Arrays.sort(times2);
+//		System.out.println("Seq-Median: " + times2[0]);
+//		
 //		long[] times4 = new long[max];
 //		for (int i = 0; i < max; i++) {
 //			pp.initializeArray(length);
